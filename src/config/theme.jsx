82a9c990bba516e6d032b6273139c233029b705f
claudioquirozipi <claudioquirozipi@gmail.com>
2020-01-16 16:12:58 -0500

    se corrigió el problema de editar y se agregó interacción a las vistas de uma

--- src/config/theme.jsx
+++ src/config/theme.jsx
@@ -11,7 +11,7 @@ const theme = {
     tertiary: "white"
   },
   shadow: "0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)",
-  guideLines: true
+  guideLines: false
 };
 
 export default theme;
--- src/view/uma/index.jsx
+++ src/view/uma/index.jsx
@@ -19,27 +19,32 @@ const UmaComponents = () => {
   const [data, setData] = useState([
     {
       id: "1",
-      text: "tarea 1"
+      text: "descripción del evento 1"
     },
     {
       id: "2",
-      text: "tarea 2"
+      text: "descripción del evento 2"
     },
     {
       id: "3",
-      text: "tarea 3"
+      text: "descripción del evento 3"
     },
     {
       id: "4",
-      text: "tarea 4"
+      text: "descripción del evento 4"
     }
   ]);
-
+  const [view, setView] = useState({
+    editar: false,
+    agregar: true,
+    card: true
+  });
   const handleOnSubmit = e => {
     e.preventDefault();
-    const newData = [...data, { text, id: data.length + 1 }];
+    const newData = [...data, { text, id: Math.floor(Math.random() * 100) }];
     console.log(newData);
     setData(newData);
+    setText("");
   };
   const handleGetName = e => {
     setText(e.target.value);
@@ -53,91 +58,113 @@ const UmaComponents = () => {
     const newData = data.filter(dato => dato.id === id);
     setIdForm(id);
     setText(newData[0].text);
+    setView({
+      editar: true,
+      agregar: false,
+      card: false
+    });
   };
   const handleEditar = e => {
     e.preventDefault();
     console.log("editando", text, idForm);
-    const newData = data.filter(dato => dato.id !== idForm);
-    setData([
-      ...newData,
-      {
-        text,
-        id: idForm
+    let newData = [];
+    let i = 0;
+    data.forEach(element => {
+      if (element.id === idForm) {
+        newData[i] = {
+          text,
+          id: idForm
+        };
+      } else {
+        newData[i] = data[i];
       }
-    ]);
+      i++;
+    });
+    console.log(newData);
+    setData(newData);
+    setText("");
+    setView({
+      editar: false,
+      agregar: true,
+      card: true
+    });
   };
   return (
     <>
       <ContainerML>
         <h1 className="cPrimary">Eventos Uma</h1>
       </ContainerML>
-      <ContainerML>
-        <Form onSubmit={handleOnSubmit} theme={theme}>
-          {/* <h3>agregar</h3> */}
-          {/* <label htmlFor="text">text</label> */}
-          <FormML.Input1>
-            <input
-              type="text"
-              id="text"
-              value={text}
-              onChange={handleGetName}
-              required
-            />
-          </FormML.Input1>
-          <ButtonML onClick={() => {}}>
-            {/* <input type="submit" value="agregar" /> */}
-            Agregar
-          </ButtonML>
-        </Form>
-      </ContainerML>
-      <ContainerML>
-        <Form onSubmit={handleEditar} theme={theme}>
-          {/* <h3>editar</h3> */}
-          {/* <label htmlFor="text">text</label> */}
-          <FormML.Input1>
-            <input
-              type="text"
-              id="text"
-              value={text}
-              onChange={handleGetName}
-              required
-            />
-          </FormML.Input1>
-          {/* <input type="submit" value="editar" /> */}
-          <ButtonML onClick={() => {}}>Editar</ButtonML>
-        </Form>
-      </ContainerML>
 
-      <ContainerML.Column>
-        {data.map(data => (
-          <CardML>
-            <CardML.Img1 src={imgCard1} />
-            <CardML.header></CardML.header>
-            <CardML.Body>
-              <h4>Evento</h4>
-              <p className="tcSecondary" key={data.id}>
-                {data.text}
-              </p>
-              <ContainerButtons>
-                <ButtonIcon
-                  color={theme.color.alert}
-                  theme={theme}
-                  onClick={() => handleDelete(data.id)}
-                >
-                  <img src={imgDelete} alt="" />
-                </ButtonIcon>
-                <ButtonIcon
-                  color={theme.color.primary}
-                  theme={theme}
-                  onClick={() => handleGetNameEditar(data.id)}
-                >
-                  <img src={imgEdit} alt="" />
-                </ButtonIcon>
-              </ContainerButtons>
-            </CardML.Body>
-          </CardML>
-        ))}
-      </ContainerML.Column>
+      {view.card ? (
+        <ContainerML.Column>
+          {data.map(data => (
+            <CardML key={data.id}>
+              <CardML.Img1 src={imgCard1} />
+              <CardML.header></CardML.header>
+              <CardML.Body>
+                <h4>Evento</h4>
+                <p className="tcSecondary">{data.text}</p>
+                <ContainerButtons>
+                  <ButtonIcon
+                    color={theme.color.alert}
+                    theme={theme}
+                    onClick={() => handleDelete(data.id)}
+                  >
+                    <img src={imgDelete} alt="" />
+                  </ButtonIcon>
+                  <ButtonIcon
+                    color={theme.color.primary}
+                    theme={theme}
+                    onClick={() => handleGetNameEditar(data.id)}
+                  >
+                    <img src={imgEdit} alt="" />
+                  </ButtonIcon>
+                </ContainerButtons>
+              </CardML.Body>
+            </CardML>
+          ))}
+        </ContainerML.Column>
+      ) : null}
+      {view.agregar ? (
+        <ContainerML>
+          <Form onSubmit={handleOnSubmit} theme={theme}>
+            {/* <h3>agregar</h3> */}
+            {/* <label htmlFor="text">text</label> */}
+            <FormML.Input1>
+              <input
+                type="text"
+                id="text"
+                value={text}
+                onChange={handleGetName}
+                required
+              />
+            </FormML.Input1>
+            <ButtonML onClick={() => {}}>
+              {/* <input type="submit" value="agregar" /> */}
+              Agregar
+            </ButtonML>
+          </Form>
+        </ContainerML>
+      ) : null}
+      {view.editar ? (
+        <ContainerML>
+          <Form onSubmit={handleEditar} theme={theme}>
+            {/* <h3>editar</h3> */}
+            {/* <label htmlFor="text">text</label> */}
+            <FormML.Input1>
+              <input
+                type="text"
+                id="text"
+                value={text}
+                onChange={handleGetName}
+                required
+              />
+            </FormML.Input1>
+            {/* <input type="submit" value="editar" /> */}
+            <ButtonML onClick={() => {}}>Editar</ButtonML>
+          </Form>
+        </ContainerML>
+      ) : null}
     </>
   );
 };
